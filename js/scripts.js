58053b5d300ad769a1627887f1b1f22f434eb74d
samfan01 <dsamfan@gmail.com>
2021-01-12 07:36:14 +0300

    added form on totals column

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+$('#take').click(function(){
+    var reg=$('#regular').val()
+    var med=$('#medium').val()
+    var lar=$('#large').val()
+    var par=$('#parmesian').val()
+    var ame=$('#american').val()
+    var sof=$('#soft').val()
+    var plus=parseInt(reg)
+    var plus1=parseInt(sof)
+    
+    $('.text').text(plus)
+})
 
 
 
