2d732f6e97e65b0304ab850159798d07606522d3
samfan01 <dsamfan@gmail.com>
2021-01-13 14:42:47 +0300

    added Project README

---
+++ README.md
@@ -0,0 +1,13 @@
+# {Lastborn pizza place}
+#### {THis is a page that processes and gives information on Akana names}, {Date of current version}
+ By **{Douglas Samphan}**
+## Description
+{This is a web Application that allows Lastborn customers order and choose their pizza for delivery.
+## Setup/Installation Requirements
+* This webpage is not to be installed.
+## Known Bugs
+{}
+## Technologies Used
+{The Mark up language used is HTML which is styled by Css and Javascript.}
+## Support and contact details
+{Reach the creater of the web page for any questions or comments through dsamfan@gmail.com}
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var order = [];
+
+
 $(document).ready(function () {
     function Regular(size, crust, topping, extras, total) {
         this.size = size;
@@ -14,19 +17,19 @@ $(document).ready(function () {
         if (this.size == 700) {
             return 'Medium';
         }
-        if (this.size ==900) {
+        if (this.size == 900) {
             return 'Large';
         }
 
     }
     Regular.prototype.Regularcrust = function () {
-        if (this.crust== 100) {
+        if (this.crust == 100) {
             return 'American';
         }
         if (this.crust == 200) {
             return 'Parmesian';
         }
-        if (this.crust==150) {
+        if (this.crust == 150) {
             return 'Soft';
         }
 
@@ -38,7 +41,7 @@ $(document).ready(function () {
         if (this.topping == 150) {
             return 'Beef';
         }
-        if (this.topping ==100) {
+        if (this.topping == 100) {
             return 'Bacon';
         }
     }
@@ -53,88 +56,89 @@ $(document).ready(function () {
             return 'Extra-Sauce';
         }
     }
+$('#take').click(function (event) {
+    event.preventDefault()
+    var pizzaSize = parseInt($('#sel1').val());
+    var pizzaCrust = parseInt($('#crust1').val());
+    var pizzaTopping = parseInt($('#topping1').val());
+    var pizzaExtra = parseInt($('#extras1').val());
+    var pizzaTotal = parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(pizzaTopping) + parseInt(pizzaExtra);
 
-    $('#take').click(function (event) {
-        event.preventDefault()
-        var pizzaSize = parseInt($('#sel1').val());
-        var pizzaCrust = parseInt($('#crust1').val());
-        var pizzaTopping = parseInt($('#topping1').val());
-        var pizzaExtra = parseInt($('#extras1').val());
-        var pizzaTotal = pizzaSize + pizzaCrust + pizzaTopping + pizzaExtra
-
-        var newPizza = new Regular(pizzaSize, pizzaCrust, pizzaTopping, pizzaExtra)
-
-        $('ul#list').append("<li><span class='contact'>" + newPizza.Regularsize() + " " + pizzaSize +
-            '<br>' + newPizza.Regularcrust() + ' ' + pizzaCrust + '<br>' + newPizza.Regulartopping() + " "
-            + pizzaTopping + '<br>' + newPizza.Regularextras() + " " + pizzaExtra + "<br>" + "Total:" + pizzaTotal + "</span></li>")
-
-
-        $('.text').text(+pizzaTotal)
-    })
-    function Veggy(size, crust, extras) {
-            this.size = size;
-            this.crust = crust;
-            this.extras = extras;
+    var newPizza = new Regular(pizzaSize, pizzaCrust, pizzaTopping, pizzaExtra);
+    var totalAmount = 0;
+    for (let i = 0; i < order.length; i++) {
+        totalAmount = + order[i].pizzaTotal;
     }
-    Veggy.prototype.Veggysize = function () {
-        if (this.size == 400) {
-            return 'Small';
-        }
-        if (this.size == 550) {
-            return 'Medium';
-        }
-        if (this.size == 700) {
-            return 'Large';
-        }
+    $('ul#list').append("<li><span class='contact'>" + newPizza.Regularsize() + " " + pizzaSize +
+        '<br>' + newPizza.Regularcrust() + ' ' + pizzaCrust + '<br>' + newPizza.Regulartopping() + " "
+        + pizzaTopping + '<br>' + newPizza.Regularextras() + " " + pizzaExtra + "<br>" + "Total:" + pizzaTotal + '<br>' +
+        'Total Amount ' + totalAmount + "</span></li>")
+});
+function Veggy(size, crust, extras) {
+    this.size = size;
+    this.crust = crust;
+    this.extras = extras;
+}
+Veggy.prototype.Veggysize = function () {
+    if (this.size == 400) {
+        return 'Small';
     }
-    Veggy.prototype.Veggycrust = function () {
-        if (this.crust== 100) {
-            return 'American';
-        }
-        if (this.crust == 200) {
-            return 'Parmesian';
-        }
-        if (this.crust==150) {
-            return 'Soft';
-        }
+    if (this.size == 550) {
+        return 'Medium';
     }
-    Veggy.prototype.Veggyextras = function () {
-        if (this.extras == 100) {
-            return 'Extra-Olives';
-        }
-        if (this.extras == 50) {
-            return 'Extra-Cheese';
-        }
-        if (this.extras == 60) {
-            return 'Extra-Sauce';
-        }
-       
+    if (this.size == 700) {
+        return 'Large';
     }
-    $('#take1').click(function () {
-        var pizzaSize1 =parseInt($('#sel2').val());
-        var pizzaCrust1 = parseInt($('#crust2').val());
-        var pizzaExtra1 = parseInt($('#extra1').val());
-        var pizzaTotal1 = pizzaSize1+pizzaCrust1+pizzaExtra1
+}
+Veggy.prototype.Veggycrust = function () {
+    if (this.crust == 100) {
+        return 'American';
+    }
+    if (this.crust == 200) {
+        return 'Parmesian';
+    }
+    if (this.crust == 150) {
+        return 'Soft';
+    }
+}
+Veggy.prototype.Veggyextras = function () {
+    if (this.extras == 100) {
+        return 'Extra-Olives';
+    }
+    if (this.extras == 50) {
+        return 'Extra-Cheese';
+    }
+    if (this.extras == 60) {
+        return 'Extra-Sauce';
+    }
 
-        var newPizza1 = new Veggy(pizzaSize1,pizzaCrust1,pizzaExtra1)
+}
+$('#take1').click(function () {
+    var pizzaSize1 = parseInt($('#sel2').val());
+    var pizzaCrust1 = parseInt($('#crust2').val());
+    var pizzaExtra1 = parseInt($('#extra1').val());
+    var pizzaTotal1 = pizzaSize1 + pizzaCrust1 + pizzaExtra1
 
-        $('ul#list').append("<li><span class='contact'>" + newPizza1.Veggysize() + " " + pizzaSize1 +
-        '<br>' + newPizza1.Veggycrust() + ' ' + pizzaCrust1 + '<br>' + newPizza1.Veggyextras() + " " + pizzaExtra1 + "<br>" + "Total:" + pizzaTotal1 + "</span></li>")
-        alert(pizzaSize1)
+    var newPizza1 = new Veggy(pizzaSize1, pizzaCrust1, pizzaExtra1)
 
-    });
-    $('#take2').click(function () {
-        $('form').show()
-    })
-    $('#sub').click(function () {
-        var customer = $('#name').val()
-        var home = $('#address').val()
-        alert("Thank you " + customer + " your order will be delivered to " + home + " in a few minutes!")
-    })
+    $('ul#list').append("<li><span class='contact'>" + newPizza1.Veggysize() + " " + pizzaSize1 +
+        '<br>' + newPizza1.Veggycrust() + ' ' + pizzaCrust1 + '<br>' + newPizza1.Veggyextras() + " " + pizzaExtra1 + "<br>" + "Total:" + pizzaTotal1 +
+        '<br>' + 'Total Amount ' + totalAmount + "</span></li>")
+    alert(pizzaSize1)
 
-
-
+});
+$('#take2').click(function () {
+    $('form').show()
 })
+$('#sub').click(function () {
+    var customer = $('#name').val()
+    var home = $('#address').val()
+    alert("Thank you " + customer + " your order will be delivered to " + home + " in a few minutes!")
+})
+
+});
+
+
 
 
 
