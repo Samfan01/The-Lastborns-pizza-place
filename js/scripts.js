3cf5ff6f59cc6bf040a041d9ddeb696dd8a6b78c
samfan01 <dsamfan@gmail.com>
2021-01-13 08:00:35 +0300

    added js to delivery

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,83 @@
+$(document).ready(function () {
+    function Regular(size, crust, topping, extras, total) {
+        this.size = size;
+        this.crust = crust;
+        this.topping = topping;
+        this.extras = extras;
+        this.total = total;
 
-$(document).ready(function(){
-$('#take').click(function(){
-    var reg=$('#regular').val()
-    var med=$('#medium').val()
-    var lar=$('#large').val()
-    var par=$('#parmesian').val()
-    var ame=$('#american').val()
-    var sof=$('#soft').val()
-    var plus=parseInt(reg)
-    var plus1=parseInt(sof)
-    var pizza=$('#sel1').val()
-    if(pizza===reg){
-        par=100
+    };
+    Regular.prototype.Regularsize = function () {
+        Regular.size == 500
+        return 'regular'
+    
+    }
+    Regular.prototype.Regularcrust = function () {
+        Regular.crust === 200
+            return 'American'
+       Regular.crust===100
+            return 'Parmesian'
         
     }
-    $('.text').text(reg+par)
-})
-$('#take2').click(function(){
-    $('form').show()
-})
+    Regular.prototype.Regulartopping = function () {
+        Regular.topping == 100
+        return 'Bacon'
+    }
+    Regular.prototype.Regularextras = function () {
+        Regular.extras == 50
+        return 'Extra cheese'
+    }
+
+    $('#take').click(function (event) {
+        event.preventDefault()
+        var pizzaSize = parseInt($('#sel1').val());
+        var pizzaCrust = parseInt($('#crust1').val());
+        var pizzaTopping = parseInt($('#topping1').val());
+        var pizzaExtra = parseInt($('#extras1').val());
+        var pizzaTotal = pizzaSize + pizzaCrust + pizzaTopping + pizzaExtra
+
+        var newPizza = new Regular(pizzaSize, pizzaCrust, pizzaTopping, pizzaExtra)
+
+        $('ul#list').append("<li><span class='contact'>" + newPizza.Regularsize() + " " + pizzaSize +
+            '<br>' + newPizza.Regularcrust() + ' ' + pizzaCrust + '<br>' + newPizza.Regulartopping() + " "
+            + pizzaTopping + '<br>' + newPizza.Regularextras() + " " + pizzaExtra + "<br>" + "Total:" + pizzaTotal + "</span></li>")
+
+
+        $('.text').text(pizza)
+    })
+    $('#take1').click(function () {
+        var pizza1 = $('#sel2').val()
+
+
+
+        $('.text').text(pizza1)
+    });
+    $('#take2').click(function () {
+        $('form').show()
+    })
+    $('#sub').click(function(){
+        var customer=$('#name').val()
+        var home=$('#address').val()
+        alert("Thank you "+customer+" your order will be delivered to "+home+" in a few minutes!")
+    })
 
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var pizza=$('#sel1').val()
+// var par=$('#parmesian').val()
+// var ame=$('#american').val()
+// var sof=$('#soft').val()
